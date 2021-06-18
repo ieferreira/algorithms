d0f8e4c6a34d72a16ff6dd951bcbc7732c1f2ea1
ieferreira <iveferreirach@unal.edu.co>
2021-06-18 17:42:06 -0500

    added binary search in python and go

---
+++ BinarySearch/binarySearch.py
@@ -0,0 +1,28 @@
+def binary_search(ls, low, high, x, calls):
+
+    if high >= low:
+        calls += 1
+        mid = (high + low) // 2
+        print(calls)
+        if ls[mid] == x:
+            return mid
+
+        elif ls[mid] > x:
+            return binary_search(ls, low, mid - 1, x)
+
+        else:
+            return binary_search(ls, mid + 1, high, x)
+
+    else:
+        return None
+
+
+ls = [2, 3, 4, 10, 14, 21, 25, 29, 32, 220, 980, 1001, 1200, 1230]
+x = 1001
+
+result = binary_search(ls, 0, len(ls)-1, x)
+
+if result is not None:
+    print(f"Element {x} is  present at index {result}")
+else:
+    print(f"Element {x} not found")
